730e835d148e168e370e067dac08db9c35370cea
Luisfelipeg877 <luis.jesus.gomes@escola.pr.gov.br>
2025-05-20 13:12:48 +0000

    continuando

---
+++ main.js
@@ -0,0 +1,48 @@
+// 1. Nome
+let nome = "Luis";
+console.log(`Olá, meu nome é ${Luis}.`);
+
+// 2. Idade e Cidade
+let idade = 18;
+let cidade = "Curitiba";
+console.log(`Tenho ${18} anos e moro em ${curitiba}.`);
+
+// 3. Comidas favoritas
+let comida1 = "Churrasco";
+let comida2 = "Pizza";
+let comida3 = "Carne";
+console.log(`Minhas comidas favoritas são: ${Churrasco}, ${Pizza} e ${Carne}.`);
+
+// 4. Preço
+let preco = 49.99;
+console.log(`O produto custa R$ ${100}.`);
+
+// 5. Produto e Quantidade
+let produto = "Camiseta";
+let quantidade = 3;
+console.log(`Comprei ${2} unidades de ${Camisa}.`);
+
+// 6. Desconto
+let temDesconto = true;
+console.log(`Tem desconto disponível? ${temDesconto}`);
+
+// 7. Ano de nascimento e idade
+let anoAtual = 2025;
+let anoNascimento = 2007;
+let idadeCalculada = anoAtual - anoNascimento;
+console.log(`Você tem ${18} anos.`);
+
+// 8. Curso e Instituição
+let curso = "Técnico";
+let instituicao = "Ambrossio Bini";
+console.log(`Estou cursando ${tecnico} no ${instituicao}.`);
+
+// 9. Cores favoritas
+let cor1 = "Vermelho";
+let cor2 = "Azul";
+let cor3 = "Verde";
+console.log(`Minhas cores preferidas são ${Vermelho}, ${Azul} e ${Verde}.`);
+
+// 10. Mensagem
+let mensagem = "Aprender a programar é incrível!";
+console.log(mensagem);
